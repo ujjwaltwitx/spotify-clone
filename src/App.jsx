3ed3d5d1b1a99e5f27f798ce08675acbac1aee97
ujjwaltwitx <ujjwalcpj123@gmail.com>
2022-03-07 10:25:01 +0530

    Last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import SideBar from './SideBar';
 import MainBody from './MainBody';
-import './css/App.css'
+import './css/App.css';
 import MainBodyFooter from './MainBodyFooter';
 import './css/MainBodyFooter.css'
 function App() {
   return (
     <div className='app'>
       <div className='app_content'>
-      <SideBar/>
-      <MainBody />
+        <SideBar/>
+        <MainBody />
       </div>
       <MainBodyFooter />
     </div>
